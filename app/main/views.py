c1de92cba151d328eb01f1b9f6ab5cb29eaaf01c
Sam Yang <samabc75@gmail.com>
2019-09-07 18:33:04 +0800

    rebuild db

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for
 from app.main.forms import New_Material_Form
 from app.main import bp
-from app.models import Material, Series
+from app.models import Lessons, Series
 from app import db
 
 @bp.route('/')
@@ -13,7 +13,7 @@ def new_material():
       form = New_Material_Form()
       if form.validate_on_submit():
          series = form.series.data
-         new_material = Material(series_id=series.id)
+         new_material = Lessons(series_id=series.id)
          note_url = form.note_url.data
          slides_url = form.slides_url.data
          db.session.add(new_material)
@@ -24,6 +24,6 @@ def new_material():
 
 @bp.route('/material_list')
 def material_list():
-   materials = Material.query.all()
+   materials = Lessons.query.all()
    
    return render_template('main/material_list.html', materials=materials)
--- app/models.py
+++ app/models.py
@@ -3,30 +3,33 @@ from . import db
 class Series(db.Model):
     __tablename__ = 'series'
     id = db.Column(db.Integer, primary_key=True, unique=True, index=True)
-    name = db.Column(db.String(64), index=True)
-    materials = db.relationship('Material', backref='series')
+    title = db.Column(db.String(64), index=True)
+    lessons = db.relationship('Lessons', backref='series')
 
     def __repr__(self):
-        return '%r. %s' % (self.id, self.name)
+        return '%r. %s' % (self.id, self.title)
 
-class Material(db.Model):
-    __tablename__ = 'materials'
+class Lessons(db.Model):
+    __tablename__ = 'lessons'
     id = db.Column(db.Integer, primary_key=True, unique=True, index=True)
     series_id = db.Column(db.Integer, db.ForeignKey('series.id'))
-    notes = db.relationship('Note', backref='material')
-    slides = db.relationship('Slides', backref='material')
+    title = db.Column(db.String(64), index=True)
+    lecturer = db.Column(db.String(64), index=True)
+    time = db.Column(db.DateTime)
+    notes = db.relationship('Notes', backref='lesson')
+    slides = db.relationship('Slides', backref='lesson')
 
     def __repr__(self):
         return '<Material %r>' % (self.topic)
 
-class Note(db.Model):
+class Notes(db.Model):
     __tablename__ = 'notes'
     id = db.Column(db.Integer, primary_key=True, unique=True, index=True)
-    url = db.Column(db.String(200), index=True)
-    material_id = db.Column(db.Integer, db.ForeignKey('materials.id'))
+    url = db.Column(db.String(200))
+    lesson_id = db.Column(db.Integer, db.ForeignKey('lessons.id'))
 
 class Slides(db.Model):
     __tablename__ = 'slides'
     id = db.Column(db.Integer, primary_key=True, unique=True, index=True)
-    url = db.Column(db.String(200), index=True)
-    material_id = db.Column(db.Integer, db.ForeignKey('materials.id'))
+    url = db.Column(db.String(200))
+    lesson_id = db.Column(db.Integer, db.ForeignKey('lessons.id'))
